07f2cd09fbdc429c876af821c627a53586a6f544
saddam1997 <saddam.husen1997@gmail.com>
2017-11-15 19:42:15 +0530

    get data for chart of BCH/GDS/EBT add const

--- api/controllers/ChartdataController.js
+++ api/controllers/ChartdataController.js
@@ -4,11 +4,11 @@
  * @description :: Server-side logic for managing chartdatas
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var statusOne = sails.config.company.statusOne;
 module.exports = {
 
-  getAllBidBCH: function(req, res) {
-    console.log("Enter into ask api getAllBid :: ");
+  getAllSuccessfullBidBCH: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
     BidBCH.find({
         status: {
           'like': statusOne
@@ -40,8 +40,8 @@ module.exports = {
         }
       });
   },
-  getAllAskBCH: function(req, res) {
-    console.log("Enter into ask api getAllBid :: ");
+  getAllSuccessfullAskBCH: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
     AskBCH.find({
         status: {
           'like': statusOne
@@ -62,6 +62,138 @@ module.exports = {
           if (allAskDetailsToExecute.length >= 1) {
             return res.json({
               asksBCH: allAskDetailsToExecute,
+              statusCode: 200
+            });
+          } else {
+            return res.json({
+              "message": "No Ask Found!!",
+              statusCode: 401
+            });
+          }
+        }
+      });
+  },
+  getAllSuccessfullBidGDS: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
+    BidGDS.find({
+        status: {
+          'like': statusOne
+        }
+      })
+      .sort('bidRate DESC')
+      .exec(function(err, allBidDetailsToExecute) {
+        if (err) {
+          console.log("Error to find ask");
+        }
+        if (!allBidDetailsToExecute) {
+          return res.json({
+            "message": "No Bid Found!!",
+            statusCode: 401
+          });
+        }
+        if (allBidDetailsToExecute) {
+          if (allBidDetailsToExecute.length >= 1) {
+            return res.json({
+              bidsGDS: allBidDetailsToExecute,
+              statusCode: 200
+            });
+          } else {
+            return res.json({
+              "message": "No Bid Found!!",
+              statusCode: 401
+            });
+          }
+        }
+      });
+  },
+  getAllSuccessfullAskGDS: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
+    AskGDS.find({
+        status: {
+          'like': statusOne
+        }
+      })
+      .sort('askRate ASC')
+      .exec(function(err, allAskDetailsToExecute) {
+        if (err) {
+          console.log("Error to find ask");
+        }
+        if (!allAskDetailsToExecute) {
+          return res.json({
+            "message": "No Bid Found!!",
+            statusCode: 401
+          });
+        }
+        if (allAskDetailsToExecute) {
+          if (allAskDetailsToExecute.length >= 1) {
+            return res.json({
+              asksGDS: allAskDetailsToExecute,
+              statusCode: 200
+            });
+          } else {
+            return res.json({
+              "message": "No Ask Found!!",
+              statusCode: 401
+            });
+          }
+        }
+      });
+  },
+  getAllSuccessfullBidEBT: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
+    BidEBT.find({
+        status: {
+          'like': statusOne
+        }
+      })
+      .sort('bidRate DESC')
+      .exec(function(err, allBidDetailsToExecute) {
+        if (err) {
+          console.log("Error to find ask");
+        }
+        if (!allBidDetailsToExecute) {
+          return res.json({
+            "message": "No Bid Found!!",
+            statusCode: 401
+          });
+        }
+        if (allBidDetailsToExecute) {
+          if (allBidDetailsToExecute.length >= 1) {
+            return res.json({
+              bidsEBT: allBidDetailsToExecute,
+              statusCode: 200
+            });
+          } else {
+            return res.json({
+              "message": "No Bid Found!!",
+              statusCode: 401
+            });
+          }
+        }
+      });
+  },
+  getAllSuccessfullAskEBT: function(req, res) {
+    console.log("Enter into ask api getAllSuccessfullBid :: ");
+    AskEBT.find({
+        status: {
+          'like': statusOne
+        }
+      })
+      .sort('askRate ASC')
+      .exec(function(err, allAskDetailsToExecute) {
+        if (err) {
+          console.log("Error to find ask");
+        }
+        if (!allAskDetailsToExecute) {
+          return res.json({
+            "message": "No Bid Found!!",
+            statusCode: 401
+          });
+        }
+        if (allAskDetailsToExecute) {
+          if (allAskDetailsToExecute.length >= 1) {
+            return res.json({
+              asksEBT: allAskDetailsToExecute,
               statusCode: 200
             });
           } else {
